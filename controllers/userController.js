763f67a560da378a0bb96df0bfdbd4f96149b546
willcline613 <willrcline.atx@gmail.com>
2023-05-29 16:02:52 -0500

    removed .populate

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,6 @@ module.exports = {
     try {
       const user = await User.findOne({ _id: req.params.userId })
         .select('-__v')
-        .populate('thought');
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
